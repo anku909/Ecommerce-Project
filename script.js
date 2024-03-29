e1a2d11153c6d0700c819da877ea6c6ec7dfe7e4
anku <ankuofficial909@gmail.com>
2024-03-29 23:38:52 +0530

    cards-div-complete-dynamic

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-fetch("https://fakestoreapi.com/products")
-  .then((res) => res.json())
-  .then((json) => console.log(json));
+let imageCardsDiv = document.querySelector(".card-div-2");
+let leftbtn = document.querySelector(".left-btn");
+let rightbtn = document.querySelector(".right-btn");
+let cardsDivScroll = document.querySelector(".all-mix-cards-div");
+
+function onLeftClick() {
+  cardsDivScroll.scrollLeft += -1000;
+}
+function onRightClick() {
+  cardsDivScroll.scrollLeft += 1000;
+}
+
+const storedetails = async () => {
+  try {
+    const fetchData = await fetch("https://fakestoreapi.com/products");
+    const jsonData = await fetchData.json();
+    loadImage(jsonData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+storedetails();
+
+function loadImage(data) {
+  console.log(data);
+  data.forEach((item) => {
+    let cardDiv = document.createElement("div");
+    cardDiv.classList.add("mix-card");
+    let image = document.createElement("img");
+    image.src = item.image;
+    image.alt = item.title;
+    cardDiv.append(image);
+    imageCardsDiv.appendChild(cardDiv);
+  });
+}
